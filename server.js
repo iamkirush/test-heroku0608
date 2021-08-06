7e447ef341b5b643ba5fc249ecc2464513adbbd9
Kirusanth <iamkirushik@gmail.com>
2021-08-06 14:45:27 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/test-proj'));
+app.use(express.static('./dist/test-proj'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/test-proj/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/test-proj/' }),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
